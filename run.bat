eb70454a2f3efc111fd81d144ff47caf446f73fa
Layria <105751836+Layria-b@users.noreply.github.com>
2023-04-13 11:48:59 +0100

    fix
    fix to delay program from openning before vrchat is open, program was opening before vrchat causing it to close

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
-START C:\"Program Files (x86)\Steam\steamapps\common\VRChat\VRchatSpotify.exe"
 START C:\"Program Files (x86)\Steam\steamapps\common\VRChat\launch.exe"
+timeout /t 10 /nobreak
+START C:\"Program Files (x86)\Steam\steamapps\common\VRChat\VRchatSpotify.exe"
