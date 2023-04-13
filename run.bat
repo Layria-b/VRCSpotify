271d95d3ea3a556809050c8417b01a085a5e367a
Layria <105751836+Layria-b@users.noreply.github.com>
2023-04-13 12:04:43 +0100

    changing method for a fix

--- run.bat
+++ run.bat
@@ -1,3 +1,2 @@
-START C:\"Program Files (x86)\Steam\steamapps\common\VRChat\launch.exe"
-timeout /t 10 /nobreak
 START C:\"Program Files (x86)\Steam\steamapps\common\VRChat\VRchatSpotify.exe"
+START C:\"Program Files (x86)\Steam\steamapps\common\VRChat\launch.exe"
